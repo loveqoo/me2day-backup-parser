a0a38e9f16cd82c0eba10fb75f89ab1c54f7f748
Anthony <gunam@coupang.com>
2016-11-29 19:04:03 +0900

    JSON 직렬화 가능하도록 수정. 선택하여 메모리에서 파일로 읽고 쓰도록 수정.

--- debug-parse-dir.js
+++ debug-parse-dir.js
@@ -6,16 +6,19 @@ context.set('mode', 'file');
 context.set('debug', true);
 context.set('directoryPath', '/Users/anthony/Documents/backup/me2day/garangnip/post');
 
-me2day.parse.directory(context, function () {
+me2day.parse.directory(context, function (post) {
     if (global.gc) {
         var heapTotal = process.memoryUsage().heapTotal;
         var heapUsed = process.memoryUsage().heapUsed;
         var heapUsedPercent = heapUsed / heapTotal * 100;
 
-        console.log('Memory: ' + heapUsedPercent + '% used');
+        //console.log('Memory: ' + heapUsedPercent + '% used');
         if (heapUsedPercent > 90) {
             global.gc();
         }
     }
+    console.log(new Date() + ': ' + post.title
+        + '(metoo: ' + post.metooPeopleIdList.length
+        + ', comment:' +  post.commentIdList.length +')');
 });
 me2day.save();
