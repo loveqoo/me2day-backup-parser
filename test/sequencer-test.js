8b8d1b76095ee9447fd1246785327ed158f3b23f
Anthony <gunam.jung@gmail.com>
2016-12-02 13:09:33 +0900

    시퀀스 디버그 모드에 따라 로그 나오도록 수정 lodash 라이브러리 추가. load 함수 삭제.

--- test/sequencer-test.js
+++ test/sequencer-test.js
@@ -1,33 +1,34 @@
 const assert = require('assert');
-const it = require('mocha').it;
-const describe = require('mocha').describe;
-const sequencer = require('../src/sequencer')();
+const sequencer = require('../src/sequencer');
+const _ = require('lodash');
+
+sequencer.debug();
 
 describe('sequencer', () => {
-    const key = 'sample-key';
-    const get = () => {
-        return new Promise((fulfill) => {
-            sequencer.get(key, (seq) => {
-                fulfill(seq);
-            });
-        });
-    };
-    it('#get()', (done) => {
-        Promise.all([get(),get(),get(),get()]).then((values)=> {
-            console.log(values);
-            done();
-        }).catch((error) => {
-            console.log(error);
-            done();
-        });
+  const key = 'sample-key';
+  const get = () => {
+    return new Promise((fulfill) => {
+      sequencer.get(key, (seq) => {
+        fulfill(seq);
+      });
     });
-    it('#get twice()', (done) => {
-        Promise.all([get(),get(),get(),get()]).then((values)=> {
-            console.log(values);
-            done();
-        }).catch((error) => {
-            console.log(error);
-            done();
-        });
+  };
+  it('#get()', (done) => {
+    Promise.all([get(), get(), get(), get()]).then((values) => {
+      assert.equal(values.length, _.uniq(values).length);
+      done();
+    }).catch((error) => {
+      console.log(error);
+      done();
     });
+  });
+  it('#get twice()', (done) => {
+    Promise.all([get(), get(), get(), get()]).then((values) => {
+      assert.equal(values.length, _.uniq(values).length);
+      done();
+    }).catch((error) => {
+      console.log(error);
+      done();
+    });
+  });
 });
