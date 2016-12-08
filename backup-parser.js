66cfe7d4579e075d6cb81e0b31c20e3893a2fa73
Anthony <gunam@coupang.com>
2016-12-09 00:40:50 +0900

    파일 분석 끝. 소스 정리.

---
+++ backup-parser.js
@@ -0,0 +1,6 @@
+'use strict';
+const backupParser = require('./index');
+
+// -- ex) node backup-parser.js ${resourcePath}
+// -- ${resourcePath} is ${backup-root}/me2day/yourId/post
+backupParser.parse(process.argv[2]);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const Dispatcher = require('./src/Dispatcher');
 
-// -- ex) node index.js ${resourcePath}
-// -- ${resourcePath} is ${backup-root}/me2day/yourId/post
-new Dispatcher(process.argv[2]).execute();
+module.exports = {
+    parse(path, callback){
+        new Dispatcher(path).execute(callback);
+    }
+};
