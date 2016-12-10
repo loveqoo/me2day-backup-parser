75e0e2776ce7a9c37034709f201dd670ed12ca21
Anthony <gunam@coupang.com>
2016-12-10 10:08:19 +0900

    파싱한 결과에 repository 연결되지 않는 문제 해결.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Dispatcher = require('./src/Dispatcher');
 const Repository = require('./src/Repository');
 const Domain = require('./src/Domain');
 const co = require('co');
+const Me2day = Domain.Me2day;
 const People = Domain.People;
 const Post = Domain.Post;
 const Tag = Domain.Tag;
@@ -14,15 +15,17 @@ module.exports = {
     },
     load(callback){
         let repository = new Repository();
+        Me2day.prototype.repository = repository;
         repository.onLoad.Post = rawText => Post.fromJSON(rawText);
         repository.onLoad.Tag = rawText => Tag.fromJSON(rawText);
         repository.onLoad.Comment = rawText => Comment.fromJSON(rawText);
         repository.onLoad.People = rawText => People.fromJSON(rawText);
         co(function*(){
-            yield repository.load('Post', 'People', 'Tag', 'Comment', callback);
+            yield repository.load('Post', 'People', 'Tag', 'Comment');
         }).catch((e)=>{
             console.log(e);
         }).then(()=>{
+            callback();
             console.log(repository.toString());
         });
         return repository;
